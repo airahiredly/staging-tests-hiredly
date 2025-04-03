6d453f5b880a24d6625f875db2839a1346d03225
jylli <jylliankok@gmail.com>
2025-04-03 15:52:36 +0800

    overwrite - false

--- cypress.config.js
+++ cypress.config.js
@@ -20,7 +20,7 @@ module.exports = defineConfig({
     reporter: 'mochawesome',
     reporterOptions: {
       reportDir: 'cypress/reports',
-      overwrite: true,
+      overwrite: false,
       html: false,
       json: true
     },
