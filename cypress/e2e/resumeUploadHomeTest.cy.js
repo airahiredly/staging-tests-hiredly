a20260d1dd95e1373a4e14ec55df5a5e0ba95a30
jiawei1034 <76866599+jiawei1034@users.noreply.github.com>
2025-04-09 15:31:35 +0800

    Update resumeUploadHomeTest.cy.js

--- cypress/e2e/resumeUploadHomeTest.cy.js
+++ cypress/e2e/resumeUploadHomeTest.cy.js
@@ -5,7 +5,7 @@ describe('Testing resume upload from homepage.', () => {
 
   it('Resume upload from homepage', () => {
     cy.get('.css-134xd8p > .MuiGrid-root > .MuiButtonBase-root').click();  //click upload resume button
-    cy.get('input[type=file]').selectFile('fixtures/resume.pdf', { force: true });//select resume file
+    cy.get('input[type=file]').selectFile('./staginng-tests-hiredly/cypress/fixtures/resume.pdf', { force: true });//select resume file
       cy.on("window.alert", (message) => {
         expect(message).to.equal("Your resume is uploaded"); //done upload
       })
