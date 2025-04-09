9f84ee0888edf2e9ee70f569970e5f1c1cfcc816
jiawei1034 <jiawei.code@gmail.com>
2025-04-09 15:11:22 +0800

    added resume upload test

---
+++ cypress/e2e/resumeUploadHomeTest.cy.js
@@ -0,0 +1,14 @@
+describe('Testing resume upload from homepage.', () => {
+  beforeEach(() => {
+      cy.visit('https://staging-my.hiredly.com'); 
+  });
+
+  it('Resume upload from homepage', () => {
+    cy.get('.css-134xd8p > .MuiGrid-root > .MuiButtonBase-root').click();  //click upload resume button
+    cy.get('input[type=file]').selectFile('cypress/e2e/staging-tests-hiredly/cypress/fixtures/resume.pdf', { force: true });//select resume file
+      cy.on("window.alert", (message) => {
+        expect(message).to.equal("Your resume is uploaded"); //done upload
+      })
+      });
+      
+  });
