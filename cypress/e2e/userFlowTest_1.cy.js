e43b90f27d446f9248b5d68bc124586f3ae4d171
jiawei1034 <76866599+jiawei1034@users.noreply.github.com>
2025-04-11 15:38:39 +0800

    Update userFlowTest_1.cy.js

--- cypress/e2e/userFlowTest_1.cy.js
+++ cypress/e2e/userFlowTest_1.cy.js
@@ -7,7 +7,7 @@ describe('Daily Regression Test - User Flow Test 1', () => {
         //check job search
         cy.get('.MuiInputBase-input').type("test");
         cy.get('[data-testid="SearchIcon"]').click();
-        cy.contains("TEST").should('exist');
+        cy.contains("test").should('exist');
 
         //check quick apply 
         cy.window().then((win) => {
@@ -15,7 +15,7 @@ describe('Daily Regression Test - User Flow Test 1', () => {
               win.location.href = url;  // Open the URL in the same tab
             });
           });
-          cy.contains("TEST").click();
+          cy.contains("test").click();
           cy.contains("Quick Apply").click();
           cy.get('.css-1uz15p4').click();
 
